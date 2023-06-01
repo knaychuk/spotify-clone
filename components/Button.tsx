61520557f9d9698638855dd66332a82d63d87c51
Kate <knaychuk@gmail.com>
2023-06-01 01:43:51 -0700

    layout complete

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,6 +6,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, className, disabled, type = "button", ...props }, ref) => {
+  
   return (
     <button type={type} disabled={disabled}  ref={ref} {...props} className={twMerge('w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition', className)}>
       {children}
