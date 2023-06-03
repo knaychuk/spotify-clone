0a6a90c62ce0a55e1e3989687d47ce9473130f19
Kate <knaychuk@gmail.com>
2023-06-02 17:41:26 -0700

    create types for useUser hook

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState } from "react";
+
+import { Database } from "@/types_db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() => createClientComponentClient<Database>())
+
+  return (
+    <div>
+      <SessionContextProvider supabaseClient={supabaseClient}>
+        {children}
+      </SessionContextProvider>
+    </div>
+  )
+}
+export default SupabaseProvider
